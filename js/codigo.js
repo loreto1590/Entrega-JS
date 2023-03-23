0520a98ab80e14847f67b180066ddde8f0380951
Loreto <lotorres.dwfsc16@bootcamp.ucamp.io>
2023-03-22 22:55:01 -0500

    sexta parte

--- js/codigo.js
+++ js/codigo.js
@@ -1,7 +1,7 @@
 const nombre = document.getElementById("nombreInput");
 const apellido = document.getElementById("apellidoInput");
 const usuarios = JSON.parse(localStorage.getItem("usuarios")) || [];
-console.log(usuarios);
+const cuerpoTabla = document.getElementById("cuerpoTabla");
 
 const agregarUsuario = () => {
   const usuario = {
@@ -11,8 +11,32 @@ const agregarUsuario = () => {
   };
 
   usuarios.push(usuario);
-  localStorage.setItem("usuarios", JSON.stringify(usuarios));    
 
-  console.log(usuario);
-  console.log(JSON.stringify());
+  localStorage.setItem("usuarios", JSON.stringify(usuarios));
+  mostrarUsuarios();
+};
+
+const mostrarUsuarios = () => {
+  cuerpoTabla.innerHTML = "";
+  usuarios.forEach((usuario) => {
+    cuerpoTabla.innerHTML += `<tr>
+        <th scope="row">${usuario.id}</th>
+        <td>${usuario.nombre}</td>
+        <td>${usuario.apellido}</td>
+        <td>
+        <button
+          type="button"
+          class="btn btn-danger"
+          onclick="eliminarUusario('${usuario.id}')"
+        >
+          Eliminar
+        </button><td>
+    </tr>`;
+  });
+};
+
+const eliminarUusario = (id) => {
+  console.log(id);
+  localStorage.setItem("usuarios", JSON.stringify(usuarios));
+  mostrarUsuarios();
 };
