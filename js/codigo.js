1d09453ea7931c881283cfd801e99486845c662c
Loreto <lotorres.dwfsc16@bootcamp.ucamp.io>
2023-03-22 22:37:58 -0500

    quinta parte

--- js/codigo.js
+++ js/codigo.js
@@ -1,6 +1,18 @@
-const botonañadir= document.getElementById("botonañadir");
-const botoneliminar= document.getElementById("botoneliminar");
+const nombre = document.getElementById("nombreInput");
+const apellido = document.getElementById("apellidoInput");
+const usuarios = JSON.parse(localStorage.getItem("usuarios")) || [];
+console.log(usuarios);
 
+const agregarUsuario = () => {
+  const usuario = {
+    id: crypto.randomUUID(),
+    nombre: nombre.value,
+    apellido: apellido.value,
+  };
 
+  usuarios.push(usuario);
+  localStorage.setItem("usuarios", JSON.stringify(usuarios));    
 
-
+  console.log(usuario);
+  console.log(JSON.stringify());
+};
