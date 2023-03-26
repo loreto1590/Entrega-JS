81009fcef6fdcd5a6a428c3133caac163e69b1a9
Loreto <lotorres.dwfsc16@bootcamp.ucamp.io>
2023-03-26 13:57:56 -0500

    JavaScript

--- js/codigo.js
+++ js/codigo.js
@@ -2,6 +2,8 @@ const nombre = document.getElementById("nombreInput");
 const apellido = document.getElementById("apellidoInput");
 const usuarios = JSON.parse(localStorage.getItem("usuarios")) || [];
 const cuerpoTabla = document.getElementById("cuerpoTabla");
+const btnEditar = document.getElementById("btnEditar");
+const btnAgregar = document.getElementById("btnAgregar")
 
 const agregarUsuario = () => {
   const usuario = {
@@ -31,12 +33,39 @@ const mostrarUsuarios = () => {
         >
           Eliminar
         </button><td>
+        <td>
+        <button
+          type="button"
+          class="btn btn-warning"
+          onclick="editarUusuario ('${usuario.id}')"
+        >
+          Editar
+        </button><td>
+  
     </tr>`;
   });
 };
 
 const eliminarUusario = (id) => {
   console.log(id);
+  const usuario = usuarios.find((usuario) => usuario.id === id);
+  const index = usuarios.indexOf(usuario);
+  usuarios.splice(index, 1);
+
   localStorage.setItem("usuarios", JSON.stringify(usuarios));
   mostrarUsuarios();
 };
+
+const editarUusuario = (id) => {
+  btnAgregar.style.display = "none";
+  btnEditar.style.display = "inline";
+const usuario = usuarios.find((usuario) => usuario.id === id )
+nombre.value = usuario.nombre;
+apellido.value = usuario.apellido;
+}
+
+const confirmarEdicion = () => {
+  console.log("Información actualizada")
+}
+
+window.addEventListener("load", mostrarUsuarios);
